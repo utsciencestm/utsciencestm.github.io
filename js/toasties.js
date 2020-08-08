df9cf2e636e2ab5a82122d135ffc0b9c7ba2b9f8
baidut <yingzhenqiang@gmail.com>
2020-08-08 09:42:34 -0500

    done toasties

---
+++ js/toasties.js
@@ -0,0 +1,134 @@
+
+function addPeopleAbove() {
+  function addThis(element) {  // , index, array
+    $('select#participants option[value='+element+']').attr('selected','selected');
+  }
+  $('#attendingSocial').val().forEach(addThis);
+  $('#preparedSpeeches').val().forEach(addThis);
+  $('#tableTopics').val().forEach(addThis);
+  $('#evaluator').val().forEach(addThis);
+  addThis($('#travelingAward').val());
+  $('#participants').multiselect('rebuild');
+}
+function updateSelect(source, target) {
+  $(target).html($(source).html()+'<option value="NA"> NA </option>');
+  $(target).val("NA");
+}
+function updateText() {
+  addPeopleAbove();
+  let s = "";
+  // let s = $('#bestSpeaker').val();
+  // s += '\n' + $('#toastmaster').val() + '\t';
+  // s += '\n' + $('#topicsmaster').val() + '\t';
+  // s += '\n' + $('#participants').val() + '\t';
+  // s += '\n' + $('#bestEvaluator').val() +'\t';
+  // s += '\n' + $('#travelingAward').val() +'\t';
+  $('form select').each(function() {console.log($(this).val());
+    s += $(this).val() +'\t\n';
+  } )
+
+  s += 'Meeting Points\tReason';
+
+  $("select#names option").each(function(){
+    // console.log($(this).val())
+    let score = 0;
+    let reason = '';
+    let v = $(this).val()
+    if ($('#attendingSocial').val().indexOf(v)!= -1) { score += 1; reason += 'attendingSocial,'; }
+    if ($('#toastmaster').val().indexOf(v)!= -1) { score += 1; reason += 'toastmaster,'; }
+    if ($('#topicsmaster').val().indexOf(v)!= -1) { score += 1; reason += 'topicsmaster,'; }
+    if ($('#generalEvaluator').val().indexOf(v)!= -1) { score += 1; reason += 'generalEvaluator,'; }
+    if ($('#preparedSpeeches').val().indexOf(v)!= -1) { score += 2; reason += 'preparedSpeeches,'; }
+    if ($('#tableTopics').val().indexOf(v)!= -1) { score += 1; reason += 'tableTopics,'; }
+    if ($('#evaluator').val().indexOf(v)!= -1) { score += 1; reason += 'evaluator,'; }
+    if ($('#bestSpeaker').val().indexOf(v)!= -1) { score += 3; reason += 'bestSpeaker,'; }
+    if ($('#bestTableTopics').val().indexOf(v)!= -1) { score += 2; reason += 'bestTableTopics,'; }
+    if ($('#bestEvaluator').val().indexOf(v)!= -1) { score += 2; reason += 'bestEvaluator,'; }
+    if ($('#travelingAward').val().indexOf(v)!= -1) { score += 2; reason += 'travelingAward,'; }
+    if ($('#participants').val().indexOf(v)!= -1) { score += 1; reason += 'participants,'; }
+    if ($('#earlyTMSignUp').val().indexOf(v)!= -1) { score += 1; reason += 'earlyTMSignUp,'; }
+    if ($('#memberSignUp').val().indexOf(v)!= -1) { score += 4; reason += 'memberSignUp,'; }
+    s += '\n' + score + '\t' + reason;
+  });
+
+  $('#myInput').val(s);
+}
+
+function init() { // call it after names are loaded
+//copies all contents of myDropDownListDiv into anotherDiv
+  $("[class^=select]").append($("#names").html());
+
+  new Choices('.selectLimit3', {
+    removeItemButton: true,
+    maxItemCount: 3,
+    searchResultLimit: 5,
+    renderChoiceLimit: 5
+  });
+
+  var multipleCancelButton = new Choices('[class^=selectMulti]', {
+    removeItemButton: true,
+    // maxItemCount:5,
+    searchResultLimit: 5,
+    renderChoiceLimit: 5
+  });
+
+  // after adding options
+  $('#participants').multiselect();
+  // $('#selectDropdowns #participants').multiselect({ buttonWidth:'100px',listWidth:'300px',maxHeight:'70vh' });
+}
+
+$(document).ready(function() {
+  // init();
+});
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_copy_clipboard2
+function copy_text() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+
+  var my_tooltip = document.getElementById("myTooltip");
+  my_tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var my_tooltip = document.getElementById("myTooltip");
+  my_tooltip.innerHTML = "Generate and copy to clipboard";
+}
+
+
+// read from google sheets 
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+var names = [];
+function listNames() {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1E9VdBM1YSS0ykQG-U8yAopu8a2c9_G9U66DG_pmBmw4',
+    // spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'dontRenameMe!A17:100',
+  }).then(function(response) {
+    var range = response.result;
+    if (range.values.length > 0) {
+      // appendPre('Names:');
+      for (i = 0; i < range.values.length; i++) {
+        var row = range.values[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        if (row[0] == "") {break;}
+        // appendPre(row[0]); // + ', ' + row[4]
+        let firstName = row[0].split(' ')[0];
+        names.push(firstName);
+        $('<option>').val(firstName).text(firstName).appendTo('#names');
+      }
+       init();
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+
+}
