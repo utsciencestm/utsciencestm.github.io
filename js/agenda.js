b6f53a1fca4ab65b7af65e9ae15310abcacb28a0
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2020-09-05 16:17:45 -0500

    firstAutoAgenda
    problemetic when there are empty cells.

---
+++ js/agenda.js
@@ -0,0 +1,115 @@
+// MUST READ
+// https://www.freecodecamp.org/news/cjn-google-sheets-as-json-endpoint/
+var data;
+var entry;
+var offset=1;
+
+$(document).ready(function() {
+  // if (isSignedIn) {
+  //   listNames();
+  //  }
+  loadData();
+});
+
+
+function whois(role) {
+  role = role.trim();
+  for (let i = 0; i < entry.length; i++) {
+    if(entry[i].content.$t.includes(role)) {
+    return  entry[i+offset].content.$t;
+    }
+  }
+}
+
+// https://html-online.com/articles/get-url-parameters-javascript/
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function loadData() {
+  // compute offset if meeting id is none
+  // 36
+
+  // var url="https://docs.google.com/spreadsheet/pub?key=p_aHW5nOrj0VO2ZHTRRtqTQ&single=true&gid=0&range=A1&output=csv";
+  var url="https://spreadsheets.google.com/feeds/cells/17Vtxbeh7Q6-ic89sWC8_U8RUoUAr6dlvi3jxADRMNQU/2/public/full?alt=json";
+  jQuery.getJSON(url, function (json) {
+    console.log(json);
+    data = json;
+    entry = data.feed.entry;
+    // console.log(whois('Date'));
+    // var meetingNo = '' +
+    if (getUrlVars()["number"] == undefined) {
+      var today = new Date();
+      // show the coming meeting (or current meeting)
+      for (let i = 0; i < entry.length; i++) {
+        if(entry[i].content.$t.trim() == 'Date') {
+          for (let j = 1; j < 10; j++) {
+            let date = new Date(entry[i+j].content.$t.trim())
+            if(today.getMonth() === date.getMonth() && today.getDate() <= date.getDate()) {
+                offset = j;
+                break;
+            }
+          }
+        }
+      }
+    } else {
+      number = getUrlVars()["number"].trim()
+      for (let i = 0; i < entry.length; i++) {
+        if(entry[i].content.$t.includes('Meeting #')) {
+          for (let j = 1; j < 10; j++) {
+            if(entry[i+j].content.$t.trim() == number) {
+                offset = j;
+                break;
+            }
+          }
+        }
+      }
+    }
+
+    // problem: if is empty, will go to next
+
+    $('#whatIsDate').html(whois('Date'));
+    $('#meetingNo').html(whois('Meeting #'));
+    $('#whoIsToastmaster').html(whois('Toastmaster'));
+    $('#whichRoom').html(whois('Room'));
+    $('#whoIsAhCounter').html(whois('Ah Counter'));
+    $('#whoIsGrammarian').html(whois('Grammarian')); // not Grammerian's word of the day, match the first one
+    $('#whoIsTimeKeeper').html(whois('Time Keeper'));
+    $('#whoIsSpeaker1').html(whois('Speaker # 1'));
+    $('#whoIsSpeaker2').html(whois('Speaker # 2'));
+    $('#whoIsSpeaker3').html(whois('Speaker # 3'));
+    $('#whoIsGeneralEvaluator').html(whois('General Evaluator'));
+    $('#whoIsEvaluator1').html(whois('Evaluator # 1'));
+    $('#whoIsEvaluator2').html(whois('Evaluator # 2'));
+    $('#whoIsEvaluator3').html(whois('Evaluator # 3'));
+    $('#whoIsTopicsMaster').html(whois('Topics Master'));
+    $('#whatWordOfTheDay').html(whois('Word of the Day'));
+    $('#whatTheme').html(whois('Theme'));
+    $('#whatSpeech1').html(whois('#1 Manual,'));
+    $('#whatSpeech2').html(whois('#2 Manual,'));
+    $('#whatSpeech3').html(whois('#3 Manual,'));
+  });
+
+  // <I>
+  // </I></FONT></FONT><FONT FACE="Arial, serif"><FONT SIZE=1 STYLE="font-size: 8pt">disillusioned
+  // </FONT></FONT><FONT FACE="Arial, serif"><FONT SIZE=1 STYLE="font-size: 8pt"><I>adj.</I></FONT></FONT></P>
+  // <P ><FONT FACE="Arial, serif"><FONT SIZE=1 STYLE="font-size: 8pt">
+
+
+// CORS problem, blocked
+// xmlhttp=new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function() {
+//   if(xmlhttp.readyState == 4 && xmlhttp.status==200){
+//     document.getElementById("display").innerHTML = xmlhttp.responseText;
+//   }
+// };
+// xmlhttp.open("GET",url,true);
+// xmlhttp.send(null);
+}
+  // $.ajax("https://docs.google.com/spreadsheet/pub?key=0Auwt3KepmdoudE1iZFVFYmlQclcxbW85YzNZSTYyUHc&single=true&gid=0&range=b5&output=csv").done(function(result){
+  //     alert(result);
+  // });
