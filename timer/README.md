acc5129e015dbffa85bad66dc8816f0250674b3c
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2020-10-24 09:12:14 -0500

    + time keeper tool

---
+++ timer/README.md
@@ -0,0 +1,75 @@
+The best Toastmasters Timer for your meetings, it's simple, fast and accurate. It was developed by Federico Navarrete of **The Leader Ship Toastmasters** in Lodz, Poland.
+
+http://theleadership.pl
+
+Feel free to use it it anywhere at anytime.
+
+http://toastmasterstimer.tk
+
+**This edition is based on the feedback provided by multiple contributors from all over the world for one year (Jan 2018 to Jan 2019).**
+
+![toastmasters](https://i.stack.imgur.com/TFTH7.jpg)
+
+This App provides you with different options, for example:
+- Question of the Day (30s).
+- 4 to 6 min (Ice-Breaker).
+- 5 to 7 min (Common).
+- 1 minute.
+- 1 to 1:30 min (Evaluator's introduction).
+- 2 to 3 min (Evaluation).
+- 5 to 6 min (General evaluation).
+- 1 to 2 min (Table topics).
+- 8 to 10 min.
+- 10 to 12 min.
+- 13 to 15 min.
+- 18 to 20 min.
+- Custom, create your own speech up to 100 hours.
+
+All speeches are recorded for sharing it with your members or guests.
+
+**Special features:**
+- **Vibrating** when a specific segment has been reached (*ONLY available in iOS, Android based devices or Windows Tablets*).
+- **Beeping** when a specific segment has been reached (*Desktop ONLY*).
+- **Clapping** when the maximum time has been exceeded (*Desktop ONLY*).
+
+**Color coding:**
+- **Light Green**, speeches that didn't reach the minimum time (-30s), but still qualified in a contest (*ONLY available in reports preview and exports to PDF*).
+- **Green**, minimum time was reached.
+- **Yellow**, optimal time was reached.
+- **Read**, maximum time was reached.
+- **Black**, speeches that exceed the maximum time (+30s or predefined) and are disqualified in a Toastmasters contest (*ONLY available in reports preview*).
+
+**Extra options:**
+- Export the agenda to PDF.
+- Dark Mode.
+- Easter Eggs.
+
+**Do you want to see it your own language?** Why don't you join to our community?
+
+https://poeditor.com/join/project/hJX2GTJNPv
+
+Also, the project is fully **open-source** and can be reused, modified or improved freely without any constrain. Feel free to hack it and share new your ideas!
+
+**Previews of the App:**
+
+![preview](https://i.stack.imgur.com/1PRPL.gif)
+
+Furthermore, you can download the compiled versions in the official stores:
+
+**Android:**
+
+https://play.google.com/store/apps/details?id=tk.supernova.tmtimer.tk.supernova.tmtimer
+
+**Windows 10:**
+
+https://www.microsoft.com/en-us/store/p/toastmasters-timer-pro/9n9rhqms0nwg
+
+**Amazon:**
+
+https://amzn.to/2LV5fOW
+
+**iOS:**
+
+Please follow these steps:
+
+![iOSInstall](https://i.stack.imgur.com/Gn4ut.gif)
---
+++ timer/browserSpecific/js/actions.js
@@ -0,0 +1,93 @@
+let audioBeepElement, audioElementClapping;
+let os = getMobileOperatingSystem();
+let nMobile = (os === "iOS" || os === "Android");
+let requestWakeLock;
+
+function browserChangeTitle(currentTime) {
+    document.title = currentTime !== '' ? `${currentTime} - Toastmasters Timer` : `Toastmasters Timer`;
+}
+
+function browserChangeFavIcon(fav){
+    document.querySelector("link[rel*='icon']").href = fav === '' ? `img/favicon-32x32.png` : `img/${fav}.png`;
+}
+
+function browserStartBeep() {
+    if (!nMobile) {
+        if (green === 1 || yellow === 1 || red === 1) {
+            audioBeepElement.play();
+            setTimeout(function() {
+                audioBeepElement.pause();
+            }, 500);
+        } else {
+            audioBeepElement.pause();
+        }
+    }
+}
+
+function browserStartVibrate() {
+    if ((green === 1 || yellow === 1 || red === 1) && hasVibrator)
+        navigator.vibrate(1000);
+}
+
+function browserStartClapping() {
+    if (!nMobile) {
+        audioElementClapping.play();
+        setTimeout(function() {
+            audioElementClapping.pause();
+        }, 1500);
+    }
+}
+
+function browserStopClapping() {
+    if (!nMobile)
+        audioElementClapping.pause();
+}
+
+function activateWakeLock() {
+	try {
+		navigator.getWakeLock("screen").then(function(wakeLock) {
+			request = wakeLock.createRequest();
+		});
+	}
+	catch (e) {	}
+}
+
+function deactivateWakeLock() {
+	try {
+		if (request)
+			request.cancel();
+	}
+	catch (e) {	}
+}
+
+if (!nMobile) {
+    audioBeepElement = document.createElement('audio');
+    audioBeepElement.src = './browserSpecific/sounds/beep.mp3';
+    audioBeepElement.load();
+
+    audioElementClapping = document.createElement('audio');
+    audioElementClapping.src = './browserSpecific/sounds/clapping.mp3';
+    audioElementClapping.load();
+    
+    btnVibrate.style.display = 'none';
+    btnShare.style.display = 'none';
+    btnEmail.style.display = 'none';
+
+    document.body.insertBefore(audioBeepElement, document.getElementById("snackbarMsg"));
+    document.body.insertBefore(audioElementClapping, document.getElementById("snackbarMsg"));
+} else {
+    btnClap.style.display = 'none';
+    btnBeep.style.display = 'none';
+}
+
+function browserExport() {
+    showSnackbar(lngObject.lblExportMsg);
+    setTimeout(function() {
+        var doc = new jsPDF('l', 'pt', 'a4');
+        var res = doc.autoTableHtmlToJson(document.getElementById("tblResults"));
+        doc.autoTable(res.columns, res.data, {
+            startY: 60
+        });
+        doc.save();
+    }, 250);
+}
